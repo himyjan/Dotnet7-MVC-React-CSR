65c71fb825613cd75096b6f53a3ba197d65b47da
himyjan <51815522+himyjan@users.noreply.github.com>
2022-10-01 00:22:17 +0800

    fix bug and format code

--- ClientApp/aspnetcore-react.js
+++ ClientApp/aspnetcore-react.js
@@ -9,11 +9,17 @@ const baseFolder =
     ? `${process.env.APPDATA}/ASP.NET/https`
     : `${process.env.HOME}/.aspnet/https`;
 
-const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)).filter(Boolean)[0];
-const certificateName = certificateArg ? certificateArg.groups.value : process.env.npm_package_name;
+const certificateArg = process.argv
+  .map((arg) => arg.match(/--name=(?<value>.+)/i))
+  .filter(Boolean)[0];
+const certificateName = certificateArg
+  ? certificateArg.groups.value
+  : process.env.npm_package_name;
 
 if (!certificateName) {
-  console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+  console.error(
+    'Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.'
+  );
   process.exit(-1);
 }
 
@@ -21,9 +27,7 @@ const certFilePath = path.join(baseFolder, `${certificateName}.pem`);
 const keyFilePath = path.join(baseFolder, `${certificateName}.key`);
 
 if (fs.existsSync('.env.development.local')) {
-  let lines = fs.readFileSync('.env.development.local')
-    .toString()
-    .split('\n');
+  let lines = fs.readFileSync('.env.development.local').toString().split('\n');
 
   let hasCert;
   let hasCertKey = false;
@@ -50,7 +54,7 @@ if (fs.existsSync('.env.development.local')) {
 } else {
   fs.writeFileSync(
     '.env.development.local',
-`SSL_CRT_FILE=${certFilePath}
+    `SSL_CRT_FILE=${certFilePath}
 SSL_KEY_FILE=${keyFilePath}`
   );
 }
